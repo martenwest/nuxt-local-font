4f56f4f40b9304c5079637a580d478526f6f50fa
Tom Westerhuis <tomwesterhuis@Toms-MacBook-Air.local>
2021-01-05 11:48:48 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">nuxt-local-font</h1>
+
+      <h2 class="subtitle">Example demonstrating local fonts</h2>
+
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+html {
+  font-family: 'Inter';
+}
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title {
+  display: block;
+  font-weight: 500;
+  font-size: 100px;
+  color: #35495e;
+}
+
+.subtitle {
+  display: block;
+  font-weight: 500;
+  font-size: 50px;
+  color: rgb(81, 161, 81);
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
